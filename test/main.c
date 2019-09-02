a2bb20160d02339184172a6c5a188d1fd880b406
vakgun <e.yigit@incotu.com>
2019-09-02 09:34:36 +0300

    Struct test!

---
+++ test/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct{
+    char urun_rengi[10];
+    char urun_malzemesi[10];
+    char urun_skt[10];
+}urun_ozellikler;
+typedef struct
+{
+    int urun_id;
+    int urun_fiyat;
+    char urun_adi[20];
+    urun_ozellikler urun_ozellikleri;
+}urun, *urun_p;
+
+int main()
+{
+    urun urun;
+
+
+    strcpy(urun.urun_ozellikleri.urun_malzemesi,"Demir");
+
+
+
+    printf("%s ikinci urun : ", urun.urun_ozellikleri.urun_malzemesi);
+    for(;;);
+    return 0;
+}
