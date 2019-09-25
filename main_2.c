16584d313f75aa4dc90d0feeefc5042c5b07b043
vakgun <e.yigit@incotu.com>
2019-09-25 16:21:07 +0300

    Second commit for existing file

--- main_2.c
+++ main_2.c
@@ -1,16 +1,6 @@
 // C program to illustrate 
 // size of struct 
-#include <stdio.h> 
-	struct A { 
-
-		// sizeof(int) = 4 
-
-		// Padding of 4 bytes 
-
-		// sizeof(double) = 8 
-		double z; 
-
-		// sizeof(short int) = 2 
+#include <stdio.h> struct A { // sizeof(int) = 4 // Padding of 4 bytes // sizeof(double) = 8 double z; // sizeof(short int) = 2 
 		short int y; 
 		// Padding of 6 bytes 
 				int x; 
