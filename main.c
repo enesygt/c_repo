5b6906cffbf0db71598886ecfb87d0f7c8e138b0
vakgun <e.yigit@incotu.com>
2019-09-26 12:17:51 +0300

    new

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
-#include"stdio.h"
+#include "stdio.h"
 
-int main()
-{
+int main() {
 
-	                enum Colour{BLACK=2 ,WHITE = 4,RED,YELLOW};
-  
-	        int a = BLACK;
-	         printf("%d",RED);
-	       for (;;);
-	          return 0;
+  enum Colour {BLACK = 2,WHITE = 4,RED,YELLOW};
+
+  int a = BLACK;
+  printf("%d",RED);
+  for (;;) {}
+  return 0;
 
 }
