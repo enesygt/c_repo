e6279653607b8787a4e015d75b8524359d16e500
vakgun <e.yigit@incotu.com>
2019-10-30 08:01:04 +0300

    Malloc example added.

---
+++ main_3.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+int main() {
+
+  enum Colour {BLACK = 2,WHITE = 4,RED,YELLOW};
+
+  int a = BLACK;
+  sprintf ("%d",RED);
+  for(;;) {}
+  if(x > y)
+
+  {
+
+    x + 5;
+    y + 6;
+  }
+
+}
--- test/main.c
+++ test/main.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef struct{
-    char urun_rengi[10];
-    char urun_malzemesi[10];
-    char urun_skt[10];
+typedef struct {
+  char urun_rengi[10];
+  char urun_malzemesi[10];
+  char urun_skt[10];
 }urun_ozellikler;
 typedef struct
 {
-    int urun_id;
-    int urun_fiyat;
-    char urun_adi[20];
-    urun_ozellikler urun_ozellikleri;
+  int urun_id;
+  int urun_fiyat;
+  char urun_adi[20];
+  urun_ozellikler urun_ozellikleri;
 }urun, *urun_p;
 
-int main()
-{
-    urun urun;
+int main() {
+  urun urun;
 
 
-    strcpy(urun.urun_ozellikleri.urun_malzemesi,"Demir");
+  strcpy(urun.urun_ozellikleri.urun_malzemesi,"Demir");
 
 
 
-    printf("%s ikinci urun : ", urun.urun_ozellikleri.urun_malzemesi);
-    for(;;);
-    return 0;
+  printf("%s ikinci urun : ", urun.urun_ozellikleri.urun_malzemesi);
+  for(;;) {}
+  return 0;
 }
